2638e98950c53434b7e8cf7620e203b8dc3e8ede
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-12-05 23:55:15 +0100

    routerlink to navbar button added

--- src/app/components/navigation/header/header.ts
+++ src/app/components/navigation/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [Navbar],
+  imports: [Navbar, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
