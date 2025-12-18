f31b9911f88795145f2ee28d038ddaac6c9ba97b
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-12-18 20:57:26 +0100

    scroll service added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Scroll } from './services/scroll';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'recipe-finder';
+  private scrollService = inject(Scroll);
 }
